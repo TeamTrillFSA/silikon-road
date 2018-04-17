ba27401f8b83709c7fd69371bddfd3f955e18c61
Fred Ma <fma1@terpmail.umd.edu>
2018-04-17 16:19:49 -0400

    Created product component

---
+++ client/components/product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const productComponent = (props) => {
+  const product = props.products.find(id => id === props.match.params.id);
+
+  return (
+    <div>
+      <div>
+        <h1>{product.name}</h1>
+        <h3>Price: {product.price}</h3>
+        <br />
+        <p>Description: {product.description}</p>
+        <button>Buy</button>
+      </div>
+      <hr />
+      <div>
+        <h2>Reviews</h2>
+      </div>
+    </div>
+  );
+}
