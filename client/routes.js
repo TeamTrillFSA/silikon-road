b719e5af0cc29020b70b109cfade0fb2681ef9f8
Fred Ma <fma1@terpmail.umd.edu>
2018-04-22 16:45:52 -0400

    Added Cart to routes and started working on Cart component

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Login, Signup, Home, UserHome, ProductList, Product } from './components';
+import { Login, Signup, Home, UserHome, Cart, ProductList, Product } from './components';
 import { me, fetchAllProducts } from './store';
 
 /**
@@ -26,6 +26,7 @@ class Routes extends Component {
         <Route path="/signup" component={Signup} />
         <Route exact path="/products" component={ProductList} />
         <Route path="/products/:id" component={Product} />
+        <Route path="/cart" component={Cart} />
         {
           isLoggedIn &&
             <Switch>
