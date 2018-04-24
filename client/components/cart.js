5a04265ad579488e091cbd0c2b4564210d7e4956
Fred Ma <fma1@terpmail.umd.edu>
2018-04-23 20:20:30 -0400

    Mostly completed component; Started work on backend

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,37 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+const range = (end) => [...Array(end)].map((_, i) => i);
+
 export const Cart = ({ order, tblWidthStyle1, tblWidthStyle2 }) => {
   return (
     <div className="cart">
-      <table>
-        <colgroup>
-          <col span="1" style={tblWidthStyle1} />
-          <col span="1" style={tblWidthStyle2} />
-          <col span="1" style={tblWidthStyle1} />
-          <col span="1" style={tblWidthStyle1} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th><span className="text-leftalign">Image</span></th>
-            <th><span className="text-centeralign">Product</span></th>
-            <th><span className="text-leftalign">Price</span></th>
-            <th><span className="text-rightalign">Quantity</span></th>
-          </tr>
-        </thead>
-        <tbody>
-          {order.products && order.products.map(product => (
-            <tr key={product.id}>
-              <td><img src={product.imageUrl} width="100" height="100" /></td>
-              <td>
-                <span>{product.name}</span>
-                <span>Shipping</span>
-              </td>
-              <td>{product.price}</td>
-              <td>1</td>
-            </tr>))}
-        </tbody>
-      </table>
+      <div id="cartTable">
+        <table>
+          <colgroup>
+            <col span="1" style={tblWidthStyle1} />
+            <col span="1" style={tblWidthStyle2} />
+            <col span="1" style={tblWidthStyle1} />
+            <col span="1" style={tblWidthStyle1} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th><span className="text-leftalign">Image</span></th>
+              <th><span className="text-centeralign">Product</span></th>
+              <th><span className="text-leftalign">Price</span></th>
+              <th><span className="text-rightalign">Quantity</span></th>
+            </tr>
+          </thead>
+          <tbody>
+            {order.products && order.products.map(product => (
+              <tr key={product.id}>
+                <td><img src={product.imageUrl} width="100" height="100" /></td>
+                <td>
+                  <span>{product.name}</span>
+                  <span>Shipping Firm: Silikon Road LLC</span>
+                  <button>Delete</button>
+                </td>
+                <td>{product.price}</td>
+                <td>
+                  <select>
+                    {range(10).map(number => <option key={number}>{number}</option>)}
+                  </select>
+                </td>
+              </tr>))}
+          </tbody>
+        </table>
+      </div>
+      <div id="checkoutSection">
+        <button>Proceed to Checkout</button>
+      </div>
     </div>
   );
 };
--- server/api/order_product.js
+++ server/api/order_product.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { OrderProduct } = require('../db/models');
+const { OrderProduct, Order } = require('../db/models');
 
 // Updating the order_product relationship; adding a product to an order
 router.post('/', (req, res, next) => {
@@ -8,5 +8,33 @@ router.post('/', (req, res, next) => {
     .catch(next);
 });
 
+router.get('/order/:orderId/product/:productId', (req, res, next) => {
+  OrderProduct.findOne({ where: { orderId: req.params.orderId, productId: req.params.orderId } })
+    .then(orderProduct => res.json(orderProduct))
+    .catch(next);
+});
+
+router.put('/order/:orderId/product/:productId', (req, res, next) => {
+  const putOrdProd = async () => {
+    const order = await Order.findById(req.params.orderId);
+    const { userId } = order;
+
+    if (req.user && req.user.id === userId) {
+      const orderProduct = await OrderProduct.findOne({
+        where: {
+          orderId: req.params.orderId,
+          productId: req.params.orderId,
+        },
+      });
+      await orderProduct.update({ quantity: res.body.quantity });
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(403);
+    }
+  };
+
+  putOrdProd()
+    .then(null, next);
+});
 
 module.exports = router;
