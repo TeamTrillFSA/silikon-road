9da079b8ea9b8784caaeca78a7ad14afd4956a23
Fred Ma <fma1@terpmail.umd.edu>
2018-04-24 12:30:39 -0400

    Styling, indentation, etc.

--- client/store/order.spec.js
+++ client/store/order.spec.js
@@ -5,38 +5,38 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
-import { postOrderThunker } from './order'; //change to orders
-import history from '../history';
+import { postOrderThunker } from './order';
 
 const middlewares = [thunkMiddleware];
 const mockStore = configureMockStore(middlewares);
 
 describe('thunk creators', () => {
-    let store;
-    let mockAxios;
+  let store;
+  let mockAxios;
 
-    const initialState = { order: {} };
+  const initialState = { order: {} };
 
-    beforeEach(() => {
-        mockAxios = new MockAdapter(axios);
-        store = mockStore(initialState);
-    });
+  beforeEach(() => {
+    mockAxios = new MockAdapter(axios);
+    store = mockStore(initialState);
+  });
 
-    afterEach(() => {
-        mockAxios.restore();
-        store.clearActions();
-    });
+  afterEach(() => {
+    mockAxios.restore();
+    store.clearActions();
+  });
 
-    describe('postOrderThunker', () => {
-        it('eventually dispatches the POST ORDER action', () => {
-            const fakeOrder = { userId: 1 }
-            mockAxios.onPost('/api/orders').replyOnce(201, fakeOrder);
-            return store.dispatch(postOrderThunker())
-            .then(() => {
-                const actions = store.getActions();
-                expect(actions[0].type).to.be.equal('POST_ORDER');
-                expect(actions[0].order).to.be.deep.equal(fakeOrder);
-            });
+  describe('postOrderThunker', () => {
+    it('eventually dispatches the POST ORDER action', () => {
+      const fakeOrder = { userId: 1 };
+      mockAxios.onPost('/api/orders').replyOnce(201, fakeOrder);
+      return store.dispatch(postOrderThunker())
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0].type).to.be.equal('POST_ORDER');
+          expect(actions[0].order).to.be.deep.equal(fakeOrder);
         });
     });
+  });
 });
+
