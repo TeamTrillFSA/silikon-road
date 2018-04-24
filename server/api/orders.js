11145caf7620c5ca7619abfdb569b451ec353304
Fred Ma <fma1@terpmail.umd.edu>
2018-04-24 19:37:13 -0400

    Added validation on Express API routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -11,21 +11,27 @@ router.get('/:id', (req, res, next) => {
       if (req.user && req.user.id === order.user.id) {
         res.json(order);
       } else {
-        res.sendStatus(401);
+        res.sendStatus(403);
       }
     })
     .catch(next);
 });
 
 router.post('/', (req, res, next) => {
-  Order.create(req.body)
-    .then(order => res.status(201).send(order))
-    .catch(next);
+  const { status, userId, addressId } = req.body;
+
+  if (req.user.id !== userId) {
+    res.statusCode(403);
+  } else {
+    Order.create({ status, userId, addressId })
+      .then(order => res.status(201).send(order))
+      .catch(next);
+  }
 });
 
 router.put('/:id', (req, res, next) => {
   Order.findById(req.params.id)
     .then(order => order.update(req.body))
     .catch(next);
-})
+});
 
