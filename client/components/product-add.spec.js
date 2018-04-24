794f9795a15422baa1d4ff7678324796d8c6378d
Jeff Gore <jgore00@gmail.com>
2018-04-24 11:35:58 -0400

    Added additional test for case if User exists but is guest; cleaned up indentation and a console.log()

--- client/components/product-add.spec.js
+++ client/components/product-add.spec.js
@@ -10,41 +10,57 @@ const adapter = new Adapter();
 enzyme.configure({ adapter });
 
 describe('Product-Add component', () => {
+  describe('Checking to see if there is a user', () => {
+    let rejectedOne;
+    let rejectedTwo;
 
-    describe('Checking to see if there is a user', () => {
-        let message;
-    
-        beforeEach(() => {
-            message = shallow(<AddProduct user={ { } } productNames={ [] } userInput={ { } } />);
-        });
-            
-        it('renders the approprite response when there is no user', () => {
-            expect(message.find('p').text()).to.be.equal('You must be a registered user to add a product.');
-        });
+    beforeEach(() => {
+      rejectedOne = shallow(<AddProduct
+        user={{}}
+        productNames={[]}
+        userInput={{}}
+      />);
+      rejectedTwo = shallow(<AddProduct
+        user={{ id: 3, firstName: 'GUEST', lastName: 'USER', isGuest: true }}
+        productNames={[]}
+        userInput={{}}
+      />);
     });
 
-    describe('It renders the right elements', () => {
-        let formWrapper;
+    it('renders the appropriate response when there is no user', () => {
+      expect(rejectedOne.find('p').text()).to.be.equal('You must be a registered user to add a product.');
+    });
 
-        beforeEach(() => {
-            formWrapper = shallow(<AddProduct user={ { id: 1 } } productNames={ [] } userInput={ { } } />);
-        });
+    it('renders the appropriate response when the user is a guest', () => {
+      expect(rejectedTwo.find('p').text()).to.be.equal('You must be a registered user to add a product.');
+    });
+  });
 
-        it('includes addProduct_input_name', () => {
-            console.log(formWrapper.find('label')[0]);
-            expect(formWrapper.find('label').at(0).text()).to.equal('Product Name');
-        });
+  describe('It renders the right elements', () => {
+    let formWrapper;
 
-        it('includes addProduct_input_price', () => {
-            expect(formWrapper.find('label').at(1).text()).to.equal('Product Price');
-        });
+    beforeEach(() => {
+      formWrapper = shallow(<AddProduct
+        user={{ id: 1, firstName: 'Fred', lastName: 'Ma', isGuest: false }}
+        productNames={[]}
+        userInput={{}}
+      />);
+    });
 
-        it('includes addProduct_input_desc', () => {
-            expect(formWrapper.find('label').at(2).text()).to.equal('Product Description');
-        });
+    it('includes addProduct_input_name', () => {
+      expect(formWrapper.find('label').at(0).text()).to.equal('Product Name');
+    });
 
-        it('includes addProduct_input_imageUrl', () => {
-            expect(formWrapper.find('label').at(3).text()).to.equal('Product Image URL');
-        });
-    })
+    it('includes addProduct_input_price', () => {
+      expect(formWrapper.find('label').at(1).text()).to.equal('Product Price');
+    });
+
+    it('includes addProduct_input_desc', () => {
+      expect(formWrapper.find('label').at(2).text()).to.equal('Product Description');
+    });
+
+    it('includes addProduct_input_imageUrl', () => {
+      expect(formWrapper.find('label').at(3).text()).to.equal('Product Image URL');
+    });
+  });
 });
